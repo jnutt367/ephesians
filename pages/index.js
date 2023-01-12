26bf800839ebd17203e1da8090f4245286728ce7
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-11 19:37:28 -0500

    Update index.js
    refactoring for modern styles

--- pages/index.js
+++ pages/index.js
@@ -15,28 +15,21 @@ export default function Home() {
         <h1 className={styles.title}>
          <a href="https://nextjs.org">Ephesians</a>
         </h1>
-        <div className={styles.flex}>
-           
-
-<a className={styles.code} href="https://the-holy-bible.vercel.app">HOME</a>
-            <a href="https://index-olive-two.vercel.app/">
-            <button className={styles.code}>
-                INDEX
-            </button>
-            </a>
-        </div>
-        <p className={styles.description}>
-        
-          <code className={styles.publisher}>&darr; Scroll &darr;</code>
-        </p>
-
-
-        <p className={styles.h2}>
-        <span className={styles.span}> &darr; Romans 1:16 &rarr; </span>
-          For I am not ashamed of the  gospel, because it is the power
-          of God that brings salvation to everyone who believes. First the Jew & then the Gentile
-        </p>
-
+         <div className={styles.hero}>
+       <Image 
+                                src="/book-matthew.jpg"
+                                alt="Jesus teaching"
+                                height={900}
+                                width={2000}
+                                priority
+                              />
+                          
+       <a href="https://the-holy-bible.vercel.app/">
+            <button className={styles.button}>HOME</button>
+       </a>
+         <a href="https://the-holy-bible.vercel.app/">
+            <button className={styles.button}>BOOKMARK</button>
+       </a>           
         <div className={styles.grid}>
           <div className={styles.card}>
             <h2>Chapter 1 &darr;</h2>
@@ -174,6 +167,7 @@ Final Greetings
             </p>
           </div>
         </div>
+</div>
       </main>
 
       <footer className={styles.footer}>

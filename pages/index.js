efc0d427561d7c2be4df326a7ee0c838d483c676
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-11 19:49:50 -0500

    Update index.js
    ephesians.jpg"

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         </h1>
          <div className={styles.hero}>
        <Image 
-                                src="/ephesians.jfif"
+                                src="/ephesians.jpg"
                                 alt="Jesus teaching"
                                 height={900}
                                 width={2000}

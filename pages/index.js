5372604afc6f8b997e45f94616584303c0a407c1
Jason Nutt (He/Him) Developer/Creator <jnutt367@gmail.com>
2023-01-22 21:46:38 -0500

    Update index.js
    vector-illustration-jesus-praying-gethsemane.avif

--- pages/index.js
+++ pages/index.js
@@ -17,13 +17,13 @@ export default function Home() {
         </h1>
          <div className={styles.hero}>
        <Image 
-                                src="/trust.webp"
+                                src="/vector-illustration-jesus-praying-gethsemane.avif
                                 alt="mug image"
                                 height={900}
                                 width={2000}
                                 priority
                               />
-                          
+                    <div className={styles.nav}>      
           <a href="https://the-holy-bible.vercel.app/">
             <button className={styles.button}> HOME </button>
        </a>
@@ -37,6 +37,7 @@ export default function Home() {
         <a href="https://phillipians.vercel.app/">
             <button className={styles.button}>NEXT > </button>
        </a>               
+                </div>
         <div className={styles.grid}>
           <div className={styles.card}>
             <h2>Chapter 1 &darr;</h2>
